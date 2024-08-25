73778ba36fb5fcfc42e806f4f02cf13f5960fc77
alsaeed3 <ali.eissa.570@gmail.com>
2024-08-25 20:27:11 +0400

    create subject files

---
+++ ex00/BitcoinExchange.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alsaeed <alsaeed@student.42abudhabi.ae>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/25 18:30:49 by alsaeed           #+#    #+#             */
+/*   Updated: 2024/08/25 19:43:09 by alsaeed          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#ifndef BITCOINEXCHANGE_HPP
+# define BITCOINEXCHANGE_HPP
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <iomanip>
+#include <sstream>
+#include <cmath>
+
+class BitcoinExchange {
+
+	private:
+		
+
+	public:	
+		
+};
+
+#endif
+	
